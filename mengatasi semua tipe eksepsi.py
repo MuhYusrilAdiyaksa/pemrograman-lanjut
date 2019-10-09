5b6a4d1de89d46a62b19c7ed475d06111a1e8930
MuhYusrilAdiyaksa <49294388+MuhYusrilAdiyaksa@users.noreply.github.com>
2019-10-09 14:53:34 +0800

    Create mengatasi semua tipe eksepsi.py

---
+++ mengatasi semua tipe eksepsi.py
@@ -0,0 +1,27 @@
+def main ():
+	# membuat judul program
+	print ("PROGRAM PEMBAGIAN BILANGAN")
+	
+	#mendefinisikan blok try...except
+	try:
+		#meminta user memasukkan bilangan
+		a = float (input("Masukkan a: "))
+		b = float (input("Masukkan b: "))
+	
+		#melakukan pembagian
+		hasil = a / b
+		
+	# mengantisipasi pembgian dengan 0
+	except (ZeroDivisionError, ValueError, KeyboardInterrupt):
+		print ("\nERROR: Anda telah melakukan " + "kesalahan")
+
+	else:
+		print("\na : ", a)
+		print ("b : ", b)
+		print ("a / b = ", hasil)
+	
+if __name__ == "__main__":
+	main () 
+
+
+
