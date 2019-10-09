84a7f85c942159dab68f9ac93166cde085547ca8
MuhYusrilAdiyaksa <49294388+MuhYusrilAdiyaksa@users.noreply.github.com>
2019-10-09 14:51:54 +0800

    Create try except2.py

---
+++ try except2.py
@@ -0,0 +1,24 @@
+def main ():
+	print ("PROGRAM PEMBAGIAN BILANGAN")
+	
+	#meminta user memasukkan bilangan
+	a = float (input("Masukkan a: "))
+	b = float (input("Masukkan b: "))
+	
+	#mendefinisikan blok try...except
+	try:
+		hasil =a / b
+	except ZeroDivisionError:
+		print ("\nERROR: Nilai b tidak boleh nol")
+
+	# menampilkan hasil
+	print("\na : ", a)
+	print ("b : ", b)
+	
+	#kode di bawah ini akan menimbulkan kesalahan 
+	#dengan tipe NameError
+	print ("a / b = ", hasil)
+	
+if __name__ == "__main__":
+	main () 
+
